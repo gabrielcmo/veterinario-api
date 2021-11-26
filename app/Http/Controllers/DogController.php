5cb6a58d36624a5ae5b8846988db8226532712da
Gabriel Oliveira <gabrieloliveira9669@gmail.com>
2021-11-26 13:45:24 -0300

    route api dog treatments done

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -7,14 +7,23 @@ use App\Models\Dog;
 
 class DogController extends Controller
 {
+    /*
+        Retorna o cachorro associado a esse ID
+    */
     function getDog($id){
         return Dog::find($id);
     }
-
+    
+    /*
+        Retorna o usuário dono do cachorro associado a esse ID
+    */
     function getDogOwner($id){
         return Dog::find($id)->user;
     }
 
+    /*
+        Retorna todos os tratamentos feitos pelo cachorro
+    */
     function getDogTreatments($id){
         return Dog::find($id)->treatments;
     }
