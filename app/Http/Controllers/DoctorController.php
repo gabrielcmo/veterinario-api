b4e738e674a06e15d1bd602675a22254b17adc7c
Gabriel Carlos Martins de Oliveira <gabriel@gabrielcmo>
2021-11-29 23:09:33 -0300

    get doctor api ok

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Doctor;
 
 class DoctorController extends Controller
 {
-    //
+    public function getDoctor($id){
+        return Doctor::find($id);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/dog/{id}', [DogController::class, 'getDog'])->name('getDog');
 Route::get('/dog/{id}/owner', [DogController::class, 'getDogOwner'])->name('getDogOwner');
 Route::get('/dog/{id}/treatments', [DogController::class, 'getDogTreatments'])->name('getDogTreatment');
+
+Route::get('/doctor/{id}', [DoctorController::class, 'getDoctor'])->name('getDoctor');
